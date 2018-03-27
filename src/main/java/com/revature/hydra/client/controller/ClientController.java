63b68afcfb655f617d9f04f97ce7111ab4afee82
Steven Caraballo <stevencaraballo26@gmail.com>
2018-03-27 11:40:35 -0400

    ok

--- src/main/java/com/revature/hydra/client/controller/ClientController.java
+++ src/main/java/com/revature/hydra/client/controller/ClientController.java
@@ -2,12 +2,17 @@ package com.revature.hydra.client.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,8 @@ import com.revature.hydra.client.service.ClientService;
 @CrossOrigin
 @EnableAutoConfiguration
 public class ClientController {
+	
+	private static final Logger log = Logger.getLogger(ClientController.class);
 	
 	private ClientService clientService;
 	
@@ -36,7 +43,8 @@ public class ClientController {
 	@RequestMapping(value = "/one/client/{id}", method = RequestMethod.GET)
     //@ResponseBody
     public ResponseEntity<Client> findOneClient(@PathVariable Integer id) {
-    	Client c = clientService.findOneById(id);
+		log.info("Finding client with clientId: " + id);
+		Client c = clientService.findOneById(id);
         return new ResponseEntity<>(c, HttpStatus.OK);
     }
     
@@ -47,8 +55,50 @@ public class ClientController {
     @RequestMapping(value = "/all/client", method = RequestMethod.GET)
     //@ResponseBody
     public ResponseEntity<List<Client>> findAllClient() {
+    	log.info("Finding all clients...");
     	List<Client> lc = clientService.findAll();
         return new ResponseEntity<>(lc, HttpStatus.OK);
     }
+
+    /**
+	 * Create client
+	 *
+	 * @param client to save
+	 * @return the response entity with saved client
+	 */
+	@RequestMapping(value = "/client/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Client> createClient(@Valid @RequestBody Client client) {
+		log.info("Saving client: " + client);
+		clientService.save(client);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	/**
+	 * Update client
+	 *
+	 * @param client to update
+	 * @return the response entity
+	 */
+	@RequestMapping(value = "/client/update", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> updateClient(@Valid @RequestBody Client client) {
+		log.info("Updating client: " + client);
+		clientService.update(client);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	/**
+	 * Delete client
+	 *
+	 * @param id 
+	 * @return the response entity
+	 */
+	@RequestMapping(value = "/client/delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteClient(@PathVariable int id) {
+		Client client = new Client();
+		client.setClientId(id);
+		log.info("Deleting client: " + id);
+		clientService.delete(client);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
 }
--- src/main/java/com/revature/hydra/client/controller/EndClientController.java
+++ src/main/java/com/revature/hydra/client/controller/EndClientController.java
@@ -2,12 +2,17 @@ package com.revature.hydra.client.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,8 @@ import com.revature.hydra.client.service.EndClientService;
 @CrossOrigin
 @EnableAutoConfiguration
 public class EndClientController {
+
+	private static final Logger log = Logger.getLogger(EndClientController.class);
 
 	private EndClientService endClientService;
 	
@@ -35,7 +42,8 @@ public class EndClientController {
 	@RequestMapping(value = "/one/endclient/{id}", method = RequestMethod.GET)
     //@ResponseBody
     public ResponseEntity<EndClient> findOneEndClient(@PathVariable Integer id) {
-    	EndClient c = endClientService.findOneById(id);
+		log.info("Finding endclient with endClientId: " + id);
+		EndClient c = endClientService.findOneById(id);
         return new ResponseEntity<>(c, HttpStatus.OK);
     }
 	
@@ -46,7 +54,51 @@ public class EndClientController {
 	@RequestMapping(value = "/all/endclient", method = RequestMethod.GET)
     //@ResponseBody
     public ResponseEntity<List<EndClient>> findAllEndClient() {
+    	log.info("Finding all endclients...");
     	List<EndClient> lc = endClientService.findAll();
         return new ResponseEntity<>(lc, HttpStatus.OK);
     }
+
+	/**
+	 * Create end client
+	 *
+	 * @param endclient to save
+	 * @return the response entity with saved endclient
+	 */
+	@RequestMapping(value = "/endclient/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<EndClient> createEndClient(@Valid @RequestBody EndClient endclient) {
+		log.info("Saving endclient: " + endclient);
+		endClientService.save(endclient);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	/**
+	 * Update end client
+	 *
+	 * @param endclient to update
+	 * @return the response entity
+	 */
+	@RequestMapping(value = "/endclient/update", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> updateEndClient(@Valid @RequestBody EndClient endclient) {
+		log.info("Updating endclient: " + endclient);
+		endClientService.update(endclient);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	/**
+	 * Delete end client
+	 *
+	 * @param id 
+	 * @return the response entity
+	 */
+	@RequestMapping(value = "/endclient/delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteEndClient(@PathVariable int id) {
+		EndClient endclient = new EndClient();
+		endclient.setEndClientId(id);
+		log.info("Deleting endclient: " + id);
+		endClientService.delete(endclient);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+
 }
